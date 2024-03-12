2e95ba473d71aa28d856e9f176bfd9cb64c47cdf
Mayexx <148670605+Mayexx@users.noreply.github.com>
2024-03-12 15:46:22 +0800

    Add files via upload

---
+++ Repitition.py
@@ -0,0 +1,66 @@
+# Repitition Statements
+# Data types allowed to be iterated: Lists, Ranges, Sets, Tuple, Dictionaries, Strings
+
+#x = range(5,11)
+# petName = ["Snowy", "Blacky", "Bruno"]
+#print(x)
+
+# for loop
+# for(initialization;condition;incrementation/decre) - JAVA
+
+#for num in x:
+#    print(num)
+
+# Slicing a lists
+    
+# for name in petName[2:0:-1]:
+#     print(name)    
+
+# Loopinhg Dictionaries
+# key: value 
+
+
+# ctrl + / - shortcut for comment
+
+# users = {
+#     "first_name" : "Johnny",
+#     "last_name" : "Tadea",
+#     "age" : 25, 
+#     "average" : 81.76,
+#     "list_subjects" : ["Programming", "Mathematics", "English"]
+# }
+
+#for key in user:
+#    print(key, ":", user[key])
+        
+# list_of_users = [{
+#     "first_name" : "Johnny",
+#     "last_name" : "Tadea",
+#     "age" : 25, 
+#     "average" : 81.76,
+#     "list_subjects" : ["Programming", "Mathematics", "English"]
+# },
+# {
+#     "first_name" : "Rose",
+#     "last_name" : "Tadea",
+#     "age" : 23, 
+#     "average" : 82.54,
+#     "list_subjects" : ["Programming", "Mathematics", "English"]
+# },
+# {
+#     "first_name" : "Angel",
+#     "last_name" : "Tadea",
+#     "age" : 27, 
+#     "average" : 86,
+#     "list_subjects" : ["Programming", "Mathematics", "English"]
+# },
+# ]    
+# for key in list_of_users:
+#     for x in key: 
+#         print(x, ":", key[x])
+
+# Looping in reversed
+
+# x = range(1,10)
+# for i in x[::-1]:
+#     print(i)
